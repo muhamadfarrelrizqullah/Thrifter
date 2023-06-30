9fd462bfc310364e7fcd11896d98918663f56075
muhamadfarrelrizqullah <farrelswag27@gmail.com>
2023-06-30 13:56:05 +0700

    Add Landing

---
+++ landing.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Landing page</title>
+    <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+    <link rel="stylesheet" href="css/landing.css">
+</head>
+<body>
+    <div id="bg">
+        </div>
+    <div id="main">
+        
+        <div id="logo"><img src="img/logo3.png" style="width: 200px; height: 200px; margin-top: 50px;"></div>
+        <div id="sign-nav">
+            <div id="signin">
+            <a href="formLogin.php">Login</a>
+            </div>
+            <div id="signup">
+            <a href="registerForm.php">Sign up</a>
+            </div>
+        </div>
+
+        <div id="main-content">
+               <div id="big-title">
+                 <h1>Thrifter Enjoyer</h1>
+                 
+               </div>
+               <div id="desc">
+               <p>Kami mengundang Anda untuk mengeksplorasi keajaiban baju bekas kami yang penuh cerita.
+                 Dalam dunia yang semakin terhubung ini, kami ingin memperkenalkan Anda pada keindahan kesederhanaan 
+                 dan gaya yang berkelanjutan. </p>
+                </div>
+                <div class="btn">
+                    Get started
+                </div>
+        </div>
+
+        
+        
+    </div>
+
+  
+</body>
+</html>
